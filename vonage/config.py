27c47642b66bee8aebcb05d4cab2e9fa410c00cc
Psycadelik <francisadrian9@hotmail.com>
2020-06-07 22:35:36 +0300

    add vonage module

---
+++ vonage/config.py
@@ -0,0 +1,30 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    APP_KEY = 'Something Secret'
+
+    # SQL ALCHEMY DATABASE - Sqlite
+    SQL_ALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, vonage.db)
+    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevConfig(Config):
+    pass
+
+
+class TestConfig(Config):
+    TESTING = True
+
+
+class ProdConfig(Config):
+    DEBUG = True
+
+
+configs = {
+    'development': DevConfig,
+    'testing': TestConfig,
+    'production': ProdConfig
+}
