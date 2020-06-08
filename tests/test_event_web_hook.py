8c9eee85a88a30567c58a5dbfc5f846d1a20fd4e
Psycadelik <francisadrian9@hotmail.com>
2020-06-08 16:15:05 +0300

    add event webhook test

---
+++ tests/test_event_web_hook.py
@@ -0,0 +1,32 @@
+import unittest
+import json
+from run import app
+
+
+class TestEventWebHook(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = app
+
+    def setUp(self):
+        with self.app.app_context():
+            self.app.testing = True
+            self.app = self.app.test_client()
+
+            self.eventData = {
+                "provider": ""
+            }
+
+            self.eventFailData = {
+                "provider": ""
+            }
+
+    def test_successful_event_web_hook(self):
+        """ test that the event web hook returns a successful response """
+        response = self.app.post('/', data=json.dumps(self.eventData), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_failed_event_web_hook(self):
+        """ test that the event web hook returns a failed response """
+        response = self.app.post('/', data=json.dumps(self.eventFailData), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
