7a17e98e8bdbdedf72085629e85f221887907069
Psycadelik <francisadrian9@hotmail.com>
2020-06-09 15:32:25 +0300

    add models

---
+++ vonage/models.py
@@ -0,0 +1,7 @@
+from vonage import db
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.Integer, index=True, unique=True)
+#     email = db.Column()
