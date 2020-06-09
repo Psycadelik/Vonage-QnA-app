54141394f6481ee631fd6ef9f6880e51ac35de02
Psycadelik <francisadrian9@hotmail.com>
2020-06-09 17:35:51 +0300

    update answer web hook

--- tests/test_answer_web_hook.py
+++ tests/test_answer_web_hook.py
@@ -23,12 +23,12 @@ class TestAnswerWebHook(unittest.TestCase):
 
     def test_successful_answer_web_hook(self):
         """ test that the answer hook returns a successful response """
-        response = self.app.post('/', data=json.dumps(self.answerData), content_type='application/json')
+        response = self.app.post('/inbound/', data=json.dumps(self.answerData), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_failed_answer_web_hook(self):
         """ test that the answer web hook returns a failed response """
-        response = self.app.post('/', data=json.dumps(self.failAnswerData), content_type="application/json")
+        response = self.app.post('/inbound/', data=json.dumps(self.failAnswerData), content_type="application/json")
         self.assertEqual(response.status_code, 400)
 
 
