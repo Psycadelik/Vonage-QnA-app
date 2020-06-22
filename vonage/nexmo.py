d7d5729661e9ffcf14641cc9773964f4fc085075
Psycadelik <francisadrian9@hotmail.com>
2020-06-22 18:06:42 +0300

    add the nexmo module

--- vonage/nexmo.py
+++ vonage/nexmo.py
@@ -1,2 +1,23 @@
 import nexmo
+import os
+from flask import jsonify
 
+
+def nexmo_sms(sms, recipient):
+    NEXMO_API_KEY = os.getenv("NEXMO_API_KEY")
+    NEXMO_API_SECRET = os.getenv("NEXMO_API_SECRET")
+
+    client = nexmo.Client(key=NEXMO_API_KEY, secret=NEXMO_API_SECRET)
+
+    responseData = client.send_message(
+        {
+            "from": "Vonage",
+            "to": recipient,
+            "text": sms,
+        }
+    )
+
+    if responseData["messages"][0]["status"] == "0":
+        return jsonify("Message sent successfully.")
+    else:
+        return jsonify(f"Message failed with error: {responseData['messages'][0]['error-text']}")
