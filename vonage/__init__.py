8077504c3c39e436a8bf41cb6be457576a38cf45
Psycadelik <francisadrian9@hotmail.com>
2020-06-28 15:28:54 +0300

    update notify and update webhooks

--- vonage/__init__.py
+++ vonage/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from vonage.config import configs
 from vonage.nexmo import nexmo_sms
-from quiz.geography_quiz import geography_quiz, history_quiz, music_quiz, gaming_quiz, movies_quiz
+from dialogflow.detect_intent_texts import detect_intent_texts
 
 
 def create_app(environment='development', test_config=None):
@@ -47,41 +47,25 @@ def create_app(environment='development', test_config=None):
     """ The notify URL is the entry point to the QnA game. Users opt in by choosing either 1(geographical questions),
     2(Historical questions),3a(Music), 3b (Games), 3c (Movies)  or 4 (Exit)"""
 
-    @app.route("/notify/", methods=['POST'])
+    @app.route("/webhooks/notify/", methods=['POST'])
     def notify_url():
         number = request.get_json().get('number')
-        notification = "Welcome to the Vonage QnA quiz app. Opt in by replying with either: 1 (Geographical Quiz)," \
-                       "2 (History quiz), 3a (Music quiz), 3b (Gaming quiz), 3c (Movies quiz) " \
-                       "or 4 to Exit"
+        notification = "Hello. You can start your quiz with quizzie-bot by sending the following keywords: hi," \
+                       " hello or vonage."
 
         return nexmo_sms(notification, number)
 
     """ This url receives a message using the vonage 2-way sms API and processes it with a reply """
     """ This is where the game logic should reside """
 
-    @app.route("/update/", methods=['POST'])
+    @app.route("/webhooks/update/", methods=['POST'])
     def update_url():
         trigger = request.get_json().get('message')
-        if trigger == '1':
-            geography_quiz(trigger)
+        project_id = os.getenv("PROJECT_ID")
+        session_id = os.getenv("SESSION_ID")
+        language_code = os.getenv("LANG_CODE")
 
-        elif trigger == '2':
-            history_quiz(trigger)
-
-        elif trigger == '3a':
-            music_quiz(trigger)
-
-        elif trigger == '3b':
-            gaming_quiz(trigger)
-
-        elif trigger == '3c':
-            movies_quiz(trigger)
-
-        elif trigger == '4':
-            res = jsonify("Thank you for trying us out. Try again some time")
-            return res
-        else:
-            return jsonify("Failed. Please start with sending play.")
+        return detect_intent_texts(project_id, session_id, trigger, language_code)
 
     from . import db
     db.init_app(app)
