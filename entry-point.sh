f38d8f07c3071146c078af3a10185d55992ca07c
Psycadelik <francisadrian9@hotmail.com>
2020-06-29 16:14:45 +0300

    update bash

--- entry-point.sh
+++ entry-point.sh
@@ -5,8 +5,8 @@ filename=$1
 
 #read the contents of phone_numbers.txt
 while read -r line;do
-  name="$line"
-  echo "PhoneNumber read from file - $name"
+  number="$line"
+  echo "PhoneNumber read from file - $number"
 done < "$filename"
 
 # direct bash to the root folder
@@ -15,6 +15,6 @@ cd ~/personal/QnA/two_way_sms_api/ || exit
 chmod +x notify_customer.py
 
 #run the python script to send the initial message to the customer
-python3 notify_customer.py --phone_number name || exit 1
+python3 notify_customer.py --phone_number number || exit 1
 
 cd ~/ || exit
