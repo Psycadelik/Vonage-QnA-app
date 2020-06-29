aec3b742cd0ae8b9efacbfa7d2c59a38ee625e12
Psycadelik <francisadrian9@hotmail.com>
2020-06-29 13:55:06 +0300

    print the output from notifying customer

--- two_way_sms_api/notify_customer.py
+++ two_way_sms_api/notify_customer.py
@@ -1,15 +1,22 @@
 #! /usr/bin/env python3
 import argparse
 import sys
+import os
+
+from dotenv import load_dotenv
 
 sys.path.append('../')
 from vonage.nexmo import nexmo_sms
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')  # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
 
 
 def notify_customer(number):
     text = "Hello. You can start your quiz with quizzie-bot by sending the following keywords: hi," \
            " hello or vonage."
-    return nexmo_sms(text, number)
+    print(nexmo_sms(text, number))
 
 
 if __name__ == "__main__":
