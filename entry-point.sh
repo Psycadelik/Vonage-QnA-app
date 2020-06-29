bdb330abf49743c785264c9f0af7bcb3011aca21
Psycadelik <francisadrian9@hotmail.com>
2020-06-29 10:34:07 +0300

    update bash file

--- entry-point.sh
+++ entry-point.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-set e
-
-# direct this script to initiate by notifying the customer to begin the quiz
-cd ~/personal/QnA/two_way_sms_api/ || exit
 
 # this is the phone number passed as argument when running the script
 filename=$1
@@ -13,7 +9,12 @@ while read -r line;do
   echo "Name read from file - $name"
 done < "$filename"
 
+# direct this script to initiate by notifying the customer to begin the quiz
+cd ~/personal/QnA/two_way_sms_api/ || exit
+
+chmod +x notify_customer.py
+
 #run the python script to send the initial message to the customer
-> python3 notify_customer.py --phone_number name || exit 1
+python3 notify_customer.py --phone_number name || exit 1
 
 cd ~/ || exit
