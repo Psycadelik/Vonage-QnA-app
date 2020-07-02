e1a7d3fcc233c97d7a491e56bb860d2ec6324cb5
Psycadelik <francisadrian9@hotmail.com>
2020-07-02 12:28:40 +0300

    fetch phonenumber from separate function

--- vonage/__init__.py
+++ vonage/__init__.py
@@ -4,6 +4,7 @@ import subprocess
 from flask import Flask, request, jsonify
 from vonage.config import configs
 from vonage.nexmo import nexmo_sms
+from vonage.fetch_phone_number import phone_number
 from dialogflowapi.intent_texts import detect_intent_texts
 from two_way_sms_api.send_sms import send_sms
 
@@ -50,7 +51,7 @@ def create_app(environment='development', test_config=None):
     """ The notify URL is the entry point to the QnA game. Users opt in by choosing either 1(geographical questions),
     2(Historical questions),3a(Music), 3b (Games), 3c (Movies)  or 4 (Exit)"""
 
-    @app.route("/webhooks/notify/", methods=['POST'])
+    @app.route("/notify/", methods=['POST'])
     def notify_url():
         number = request.get_json().get('number')
         notification = "Hello. You can start your quiz with quizzie-bot by sending the following keywords: hi," \
@@ -61,18 +62,31 @@ def create_app(environment='development', test_config=None):
     """ This url receives a message using the vonage 2-way sms API and processes it with a reply """
     """ This is where the game logic should reside """
 
-    @app.route("/webhooks/update/", methods=['POST'])
+    @app.route("/update/", methods=['POST'])
     def update_url():
         trigger = request.get_json().get('message')
+        project_id = os.getenv("PROJECT_ID")
+        session_id = os.getenv("SESSION_ID")
+        language_code = os.getenv("LANG_CODE")
 
-        args = "~/Downloads/updated-vonage-quizzie.json"
+        response = detect_intent_texts(project_id, session_id, trigger, language_code)
+        phone = phone_number()
 
-        # file = os.path.dirname(os.path.abspath(__file__))
+        return send_sms(response, phone)
+        # file =
         # number = open(file, 'r')
         # phone = number.readlines()[0]
-        subprocess.check_call(
-            "./set_google_creds.sh {} {}".format(args, trigger),
-            shell=True)
+
+
+
+        # file = os.path.dirname(os.path.abspath(__file__))
+
+        # command = "cd ~/personal/QnA/dialogflowapi/"
+        # cmd = "set_google_creds.sh {}".format(trigger)
+        # process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        # process2 = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        # output, error = process.communicate()
+        # output, error2 = process2.communicate()
 
     from . import db
     db.init_app(app)
