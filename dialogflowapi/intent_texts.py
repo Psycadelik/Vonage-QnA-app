4e1e820d4ddff06f036487bdc43086c4bf66be1f
Psycadelik <francisadrian9@hotmail.com>
2020-07-07 16:13:00 +0300

    clean up unsued imports

--- dialogflowapi/intent_texts.py
+++ dialogflowapi/intent_texts.py
@@ -27,8 +27,6 @@ Examples:
 
 import argparse
 import uuid
-import os
-
 import sys
 
 sys.path.append('../')
@@ -56,14 +54,6 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
             session=session, query_input=query_input)
 
         return response.query_result.fulfillment_text
-
-        # print('=' * 20)
-        # print('Query text: {}'.format(response.query_result.query_text))
-        # print('Detected intent: {} (confidence: {})\n'.format(
-        #     response.query_result.intent.display_name,
-        #     response.query_result.intent_detection_confidence))
-        # print('Fulfillment text: {}\n'.format(
-        #     response.query_result.fulfillment_text))
 
 
 # [END dialogflow_detect_intent_text]
